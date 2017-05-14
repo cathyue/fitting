4a52a1949d57ab3e6bac845585dee8a67e9062a6
cathyue <zhangxythu@hotmail.com>
2017-05-14 23:56:24 +0800

    a1a2 nonlinear equations solved

--- Exp_para.m
+++ Exp_para.m
@@ -50,26 +50,55 @@ plot(xfit, yfit);
 %% P2max
 P2max = 384.31867e-12;  %W
 ke2gsq_k2sq = P2max./(kappa1_ex.^2.*Pin_min.^2./((kappa1_ex+kappa1_in)./2).^4);
-ke2gsq_assume = ke2gsq_k2sq.*x(1).^2;
+k2_2_assume = x(1);     %(kp2_ex+kp2_in)/2
+ke2gsq_assume = ke2gsq_k2sq.*k2_2_assume.^2;
 ke2_assume = 2*pi.*0.7934e6;
 gsq_assume = ke2gsq_assume./ke2_assume;
 
 %% Pin, critical
-% chasing = (2w10-w20)/(B2-2B1)
+% chasing = (2w10-w20)/(-B2+2B1)
 chasing = Pin_min.*kappa1_ex./((kappa1_in+kappa1_ex)./2).^2;
 B2_assume = -B2r./r;
-D_w = chasing.*(B2_assume-B1.*2);
+D_w = chasing.*(-B2_assume+B1.*2);
 
 %% power line 
 % g*a2a1* << B1|a1|^2*a1, ignored in actual calculation
-fun1 = @(win, Pin, xa1sq)xa1sq.*((win-w10-B1.*xa1sq).^2+((kappa1_in+kappa1_ex)./2).^2)-kappa1_ex.*Pin;
+% options = optimoptions('fsolve','MaxFunEval', 400,'MaxIter', 1000, 'algorithm', 'levenberg-marquardt');
+% fun1 = @(win, Pin, xa1sq)xa1sq.*((win-w10+B1.*xa1sq).^2+((kappa1_in+kappa1_ex)./2).^2)-kappa1_ex.*Pin;
+w20 = 2.*w10-D_w;
 Pini = Pump_conv.*150e-6;
-lambin_pool = (lamb0+linspace(0, 0.25, 200).*1e-9).';
-win_pool = c0./lambin_pool.*2*pi;
-x0 = kappa1_ex.*Pini./((kappa1_ex+kappa1_in)./2).^2;
-a1sq_solu = zeros(length(win_pool),1);
-for k = 1:length(win_pool)
-    a1sq_solu = fsolve(fun1(win_pool(k), Pini))
-end
+lambin_pool = (lamb0+linspace(-0.1, 0.25, 2000).*1e-9).';
+win_pool = w20+[-2:0.0001:2].'.*1e10;%c0./lambin_pool.*2*pi;
+% x0 = kappa1_ex.*Pini./((kappa1_ex+kappa1_in)./2).^2;
+% a1sq_solu = zeros(length(win_pool),1);
+A = B1.^2;
+B = 2.*B1.*(win_pool-w10);
+C = ((win_pool-w10).^2+((kappa1_ex+kappa1_in)./2).^2);
+D = -kappa1_ex.*Pini;
+Delt0 = B.^2-3.*A.*C;
+Delt1 = 2.*B.^3-9.*A.*B.*C+27.*A.^2.*D;
+CC1 = ((Delt1+sqrt(Delt1.^2-4.*Delt0.^3))./2).^(1/3);
+CC2 = CC1.*(-1+sqrt(3).*1i)./2;
+CC3 = CC1.*(-1-sqrt(3).*1i)./2;
+x1 = -1./(3.*A).*(B+CC1+Delt0./CC1);
+x2 = -1./(3.*A).*(B+CC2+Delt0./CC2);
+x3 = -1./(3.*A).*(B+CC3+Delt0./CC3);
+a1sq_solu = max([real(x1),real(x2),real(x3)].').';
+a1sq_solu_min = min([real(x1),real(x2),real(x3)].').';
+a1sq_solu(real(x2)==real(x3)) = a1sq_solu_min(real(x2)==real(x3));
+
+
+A2 = B2_assume.^2;
+B2 = 2.*B2_assume.*(2.*win_pool-w20);
+C2 = ((2.*win_pool-w20).^2+(k2_2_assume).^2);
+D2 = -gsq_assume.*a1sq_solu.^2;
+Delt20 = B2.^2-3.*A2.*C2;
+Delt21 = 2.*B2.^3-9.*A2.*B2.*C2+27.*A2.^2.*D2;
+CC21 = ((Delt21+sqrt(Delt21.^2-4.*Delt20.^3))./2).^(1/3);
+CC22 = CC21.*(-1+sqrt(3).*1i)./2;
+CC23 = CC21.*(-1-sqrt(3).*1i)./2;
+x21 = -1./(3.*A).*(B+CC21+Delt0./CC21);
+x22 = -1./(3.*A).*(B+CC22+Delt0./CC22);
+x23 = -1./(3.*A).*(B+CC23+Delt0./CC23);
 
 
